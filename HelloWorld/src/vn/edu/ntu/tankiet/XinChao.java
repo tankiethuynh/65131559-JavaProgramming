44804853d8bc689d6bd29cbd6f8b76ed1f79781f
tankiethuynh <huynhkiet24905@gmail.com>
2026-01-27 22:26:26 +0700

    Hello World

---
+++ HelloWorld/src/vn/edu/ntu/tankiet/XinChao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package vn.edu.ntu.tankiet;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("Tôi đang học lập trình Java và đang thực hành GIT.");
+	}
+
+}
