fc62c18d72b418b5fd8d13e6931dd53a57898e6e
tankiethuynh <huynhkiet24905@gmail.com>
2026-02-06 21:07:41 +0700

    chuyển từ độ C sang độ F

---
+++ ChuyenDoiNhietDo/src/nhietdo.java
@@ -0,0 +1,10 @@
+
+public class nhietdo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 double celsius = 30.0;
+	     double fahrenheit = (celsius * 9/5) + 32;
+	     System.out.println(celsius + "°C = " + fahrenheit + "°F");
+	}
+}
