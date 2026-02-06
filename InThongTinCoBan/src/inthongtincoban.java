b58ed9a32763e272ebd83aa5649863d270492616
tankiethuynh <huynhkiet24905@gmail.com>
2026-02-06 09:18:10 +0700

    in thông tin của sinh viên

---
+++ InThongTinCoBan/src/inthongtincoban.java
@@ -0,0 +1,9 @@
+
+public class inthongtincoban {
+
+	public static void main(String[] args) {
+		System.out.println("Chào mừng đến với lập trình Java");
+        System.out.println("Họ tên: Huỳnh Tấn Kiệt");
+        System.out.println("Lớp: 65.CNTT-1");
+	}
+}
