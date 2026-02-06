b28a7c234019dff7d1245740d13c8634551ec6a6
tankiethuynh <huynhkiet24905@gmail.com>
2026-02-06 21:03:25 +0700

    khai báo và tính toán các phép tính cơ bản

---
+++ KhaiBaoTinhToan/src/tinhtoan.java
@@ -0,0 +1,21 @@
+
+public class tinhtoan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 15;
+        int b = 4;
+        double c = 3.5;
+        
+        // 2. Tính toán
+        int tong = a + b;
+        double thuong = a / c;
+        int phanDu = a;
+        
+        // 3. In kết quả
+        System.out.println("Tổng: " + tong);
+        System.out.println("Thương: " + thuong);
+        System.out.println("Phần dư: " + phanDu);
+	}
+
+}
