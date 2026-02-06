9ff879a26c7ac8e0a706d226b495706c39cd7fc6
tankiethuynh <huynhkiet24905@gmail.com>
2026-02-06 09:16:07 +0700

    thực hiện các phép tính +, -, *, /

---
+++ TinhToanCoBan/src/tinhtoancoban.java
@@ -0,0 +1,10 @@
+
+public class tinhtoancoban {
+	
+	public static void main(String[] args) {
+		System.out.println("2 + 1 = " + (2 + 1));
+		System.out.println("6 - 4 = " + (6 - 4));
+		System.out.println("5 * 2 = " + (5 * 2));
+		System.out.println("10 / 2 = " + (10 / 2));
+	}
+}
