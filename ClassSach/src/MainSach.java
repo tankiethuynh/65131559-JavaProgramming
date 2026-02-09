7fa1e2faf9a890eb1cc7ab6b217403f775a8b221
tankiethuynh <huynhkiet24905@gmail.com>
2026-02-09 21:37:42 +0700

    hiển thị thông tin sách

---
+++ ClassSach/src/MainSach.java
@@ -0,0 +1,15 @@
+public class MainSach {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sach sach = new Sach();
+		sach.tieuDe = "Dế Mèn Phiêu Lưu Ký";
+		sach.tacGia = "Tô Hoài";
+		sach.gia = 135000;
+		sach.soTrang = 144;
+		
+		System.out.println("===THÔNG TIN SÁCH==");
+		sach.hienThiThongTin();
+	}
+
+}
---
+++ ClassSach/src/Sach.java
@@ -0,0 +1,13 @@
+public class Sach {
+	String tieuDe;
+	String tacGia;
+	int gia;
+	int soTrang;
+	
+	void hienThiThongTin() {
+		System.out.println("Tieu de: " + tieuDe);
+		System.out.println("Tac gia: " + tacGia);
+		System.out.println("Gia: " + gia);
+		System.out.println("So trang: " + soTrang);
+	}
+}
