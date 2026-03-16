eb1b39a7644c5226c41d0218e1345ee666b24b9e
tankiethuynh <huynhkiet24905@gmail.com>
2026-03-16 15:50:33 +0700

    ví dụ arraylist số nguyên trên lớp

---
+++ VD_ArrayList_SoNguyen/src/TestMain.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> danhSachSoNguyen = new ArrayList<>();
+        Scanner banPhim = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Mời nhập phần tử thứ " + i + ": ");
+            int tam = banPhim.nextInt();
+            danhSachSoNguyen.add(tam);
+        }
+
+        System.out.println("Danh sách vừa nhập là: ");
+        for (Integer x : danhSachSoNguyen) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+
+        int soLuongChan = 0;
+        for (Integer x : danhSachSoNguyen) {
+            if (x % 2 == 0) {
+                soLuongChan++;
+            }
+        }
+        System.out.println("Số phần tử chẵn là: " + soLuongChan);
+        int tong = 0;
+        for (Integer x : danhSachSoNguyen) {
+            tong += x;
+        }
+        System.out.println("Tổng tất cả các phần tử là: " + tong);
+    }
+}
+
