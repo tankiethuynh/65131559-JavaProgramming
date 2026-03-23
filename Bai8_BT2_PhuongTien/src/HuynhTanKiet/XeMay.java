24b470fd6b47e8541f5cedad8093bf574a8362ef
tankiethuynh <huynhkiet24905@gmail.com>
2026-03-23 23:36:41 +0700

    Ứng dụng kế thừa lớp PhuongTien

---
+++ Bai8_BT2_PhuongTien/src/HuynhTanKiet/XeMay.java
@@ -0,0 +1,36 @@
+package HuynhTanKiet;
+
+public class XeMay extends PhuongTien{
+	public enum LoaiXe { so, tayGa }
+	 
+    private int dungTichXiLanh;
+    private LoaiXe loaiXe;
+ 
+    public XeMay(String hangSanXuat, int namSanXuat, double giaBan,
+                 int dungTichXiLanh, LoaiXe loaiXe) {
+        super(hangSanXuat, namSanXuat, giaBan);
+        this.dungTichXiLanh = dungTichXiLanh;
+        this.loaiXe = loaiXe;
+    }
+    
+    @Override
+    public int layVanTocToiDa() {
+        if (loaiXe == LoaiXe.so) {
+            return dungTichXiLanh >= 150 ? 130 : 100;
+        } else {
+            return dungTichXiLanh >= 150 ? 120 : 90;
+        }
+    }
+
+    public int laySoChoNgoi() {
+        return 2;
+    }
+ 
+    @Override
+    public void hienThiThongTin() {
+        super.hienThiThongTin();
+        System.out.printf ("  Dung tích xi-lanh : %d cc%n", dungTichXiLanh);
+        System.out.println("  Loại xe : " +
+            (loaiXe == LoaiXe.so ? "Xe số" : "Xe tay ga"));
+    }
+}
