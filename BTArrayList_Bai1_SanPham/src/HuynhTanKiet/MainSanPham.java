b1fc14056b8c978e0d1735922a7068e7df6044c2
tankiethuynh <huynhkiet24905@gmail.com>
2026-03-31 23:01:32 +0700

    Bài tập ArrayList nhập dữ liệu bằng Scanner

---
+++ BTArrayList_Bai1_SanPham/src/HuynhTanKiet/MainSanPham.java
@@ -0,0 +1,20 @@
+package HuynhTanKiet;
+
+public class MainSanPham {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SanPham sp1 = new SanPham();
+        
+        System.out.println("Mời nhập thông tin sản phẩm:");
+        sp1.nhap();
+        
+        System.out.println("\nThông tin sản phẩm vừa nhập:");
+        sp1.xuat();
+        
+        SanPham sp2 = new SanPham("Laptop", 20000000, 1000000);
+        System.out.println("\nThông tin sản phẩm có sẵn:");
+        sp2.xuat();
+	}
+
+}
